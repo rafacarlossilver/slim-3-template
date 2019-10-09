09732de0192e4a0336df3e49d947d063f4ddf681
Rafael <rafacarlossilver@yahoo.com.br>
2019-10-09 10:16:24 -0300

    ajustado o retorno dos erros

--- container.php
+++ container.php
@@ -4,7 +4,16 @@
 */
 
 
-#$container = $app->Container();
+$container = $app->getContainer();
+
+$container['db'] = function ($config) {
+
+    $db = $config['settings']['db'];
+    $pdo = new PDO('mysql:host=' . $db['host'] . ';dbname=' . $db['dbname'], $db['user'], $db['pass']);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    return $pdo;
+};
 
 
 // Register component on container
@@ -18,6 +27,10 @@ $container['view'] = function ($container) {
     return $view;
 };
 
-
-
-//$container['view'] = new \Slim\Views\PhpRenderer('resources/views/');
+$container['notFoundHandler'] = function ($c) {
+    return function ($request, $response) use ($c) {
+        return $c['response']
+            ->withStatus(404)
+            ->withJson(array('error'=>'404 Pagina nao encontrada'), 404);
+    };
+};
--- index.php
+++ index.php
@@ -3,14 +3,15 @@ error_reporting(E_ALL | E_STRICT);
 ini_set('display_errors','On');
 
 
+
 require 'vendor/autoload.php';
 require 'config/constants.php';
 require 'config/config.php';
 
 
-$container = new \Slim\Container($config);
-$app = new \Slim\App($container);
+
+$app = new \Slim\App(['settings' => $config]);
 include_once "container.php";
-include_once"app/routes.php";
+include_once "app/routes.php";
 
 $app->run();
